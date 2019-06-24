fa06b620fed6647609a05314dd12d3a0ad022c09
Dmitry Tarasov <dmitry.tarasov@live.com>
2019-06-24 14:19:57 +0500

    + 26. Remove Duplicates from Sorted Array

---
+++ 26. Remove Duplicates from Sorted Array/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _26._Remove_Duplicates_from_Sorted_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object res;
+            int[] a;
+
+            res = Invoker.Invoke(a = new int[] { 1, 1, 2 });
+            res = Invoker.Invoke(a = new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 });
+            res = Invoker.Invoke(a = new int[] { 1, 1, 1 });
+            res = Invoker.Invoke(a = new int[] { 1, 2, 2, 2 });
+            res = Invoker.Invoke(a = new int[] { 0, 0, 0, 0, 0, 1, 2, 2, 3, 3, 4, 4 });
+        }
+    }
+}
---
+++ 26. Remove Duplicates from Sorted Array/Solution.cs
@@ -0,0 +1,31 @@
+﻿public class Solution
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        if (nums == null || nums.Length == 0)
+        {
+            return 0;
+        }
+
+        int c = 0, k = 1;
+
+        for (int i = 0; i < nums.Length - 1 - c; i++)
+        {
+            for (int j = k; j < nums.Length; j++)
+            {
+                if (nums[i] == nums[j])
+                {
+                    c++;
+                }
+                else
+                {
+                    nums[i + 1] = nums[j];
+                    k = j + 1;
+                    break;
+                }
+            }
+        }
+
+        return nums.Length - c;
+    }
+}

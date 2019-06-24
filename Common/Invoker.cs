41073b81b6ab10e80d9c84d6e1db616c1a7a8113
Dmitry Tarasov <dmitry.tarasov@live.com>
2019-06-24 14:15:22 +0500

    Add project files.

---
+++ Common/Invoker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+public static partial class Invoker
+{
+    public static object Invoke(params object[] args)
+    {
+        Assembly assembly = Assembly.GetCallingAssembly();
+
+        var types = assembly.GetTypes();
+
+        var type = types.Where(t => t.IsClass && t.Name == "Solution").First();
+
+        var instance = Activator.CreateInstance(type);
+
+        var method = instance.GetType().GetMethods()[0];
+
+        return method.Invoke(instance, args);
+    }
+}

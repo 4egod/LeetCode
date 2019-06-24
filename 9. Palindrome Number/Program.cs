4e7cc90b79d7363b396845742a3a0e4ad0055286
Dmitry Tarasov <dmitry.tarasov@live.com>
2019-06-24 15:31:28 +0500

    9. Palindrome Number

---
+++ 9. Palindrome Number/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _9._Palindrome_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object res;
+
+            res = Invoker.Invoke(121);
+            res = Invoker.Invoke(-121);
+            res = Invoker.Invoke(10);
+        }
+    }
+}
---
+++ 9. Palindrome Number/Solution.cs
@@ -0,0 +1,24 @@
+﻿public class Solution
+{
+    public bool IsPalindrome(int x)
+    {
+        if (x < 0) return false;
+
+        int a = x, y = 0, z = 0;
+
+        do
+        {
+            y = x % 10;
+            x /= 10;
+
+            if (z > int.MaxValue / 10 || (z == int.MaxValue / 10 && y > 7)) return false;
+            if (z < int.MinValue / 10 || (z == int.MinValue / 10 && y < -8)) return false;
+
+            z *= 10;
+            z += y;
+
+        } while (x != 0);
+
+        return a == z;
+    }
+}

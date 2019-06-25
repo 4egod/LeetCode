1bcc503561718c21d527a10ce187d2eabfd7fbb8
Dmitry Tarasov <dmitry.tarasov@live.com>
2019-06-25 13:40:00 +0500

    27. Remove Element

---
+++ 27. Remove Element/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _27.Remove_Element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a;
+            object res;
+
+            res = Invoker.Invoke(a = new int[] { 3, 2, 2, 3 }, 3);
+            res = Invoker.Invoke(a = new int[] { 0, 1, 2, 2, 3, 0, 4, 2 }, 2);
+            res = Invoker.Invoke(a = new int[] { 1 }, 1);
+            res = Invoker.Invoke(a = new int[] { 4, 5 }, 5);
+        }
+    }
+}
---
+++ 27. Remove Element/Solution.cs
@@ -0,0 +1,35 @@
+﻿public class Solution
+{
+    public int RemoveElement(int[] nums, int val)
+    {
+        int c = 0;
+        for (int i = 0; i <= nums.Length - 1 - c; i++)
+        {
+            if (nums[i] == val)
+            {
+                for (int j = nums.Length - 1 - c; j >= 1; j--)
+                {
+                    if (i == j)
+                    {
+                        break;
+                    }
+
+                    if (nums[j] == val)
+                    {
+                        c++;
+                        continue;
+                    }
+                    else
+                    {
+                        nums[i] = nums[j];
+                        break;
+                    }
+                }
+
+                c++;
+            }
+        }
+
+        return nums.Length - c;
+    }
+}

bfa5ca665005d4a990d38386a2eb60290f8d9bf5
Dmitry Tarasov <dmitry.tarasov@live.com>
2019-06-25 12:47:53 +0500

    20. Valid Parentheses

---
+++ 20. Valid Parentheses/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _20._Valid_Parentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object res;
+
+            res = Invoker.Invoke("()"); // true
+            res = Invoker.Invoke("()[]{}"); // true
+            res = Invoker.Invoke("(]"); // false
+            res = Invoker.Invoke("([)]"); // false
+            res = Invoker.Invoke("{[]}"); // true
+            res = Invoker.Invoke("["); // false
+            res = Invoker.Invoke("[([]])"); // false
+            res = Invoker.Invoke("([)"); // false
+            res = Invoker.Invoke("{}{}()[]"); // true;
+            res = Invoker.Invoke("]"); // false
+
+        }
+    }
+}
---
+++ 20. Valid Parentheses/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+public class Solution
+{
+    public bool IsValid(string s)
+    {
+        Stack<char> x = new Stack<char>();
+
+        foreach (var item in s)
+        {
+            switch (item)
+            {
+                case '(':
+                    x.Push(')');
+                    break;
+                case '[':
+                    x.Push(']');
+                    break;
+                case '{':
+                    x.Push('}');
+                    break;
+                default:
+                    if (x.Count == 0 || x.Pop() != item)
+                    {
+                        return false;
+                    }
+
+                    break;
+            }
+        }
+
+        return x.Count == 0;
+    }
+}

385b746f77fac215d14f6f75f85fa8ea4b843d44
Dmitry Tarasov <dmitry.tarasov@live.com>
2019-06-25 14:37:02 +0500

    35. Search Insert Position

---
+++ 35. Search Insert Position/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _35.Search_Insert_Position
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object res;
+
+            res = Invoker.Invoke(new int[] { 1, 3, 5, 6 }, 5);
+            res = Invoker.Invoke(new int[] { 1, 3, 5, 6 }, 2);
+            res = Invoker.Invoke(new int[] { 1, 3, 5, 6 }, 7);
+            res = Invoker.Invoke(new int[] { 1, 3, 5, 6 }, 0);
+            res = Invoker.Invoke(new int[] { 1 }, 2);
+        }
+    }
+}
---
+++ 35. Search Insert Position/Solution.cs
@@ -0,0 +1,28 @@
+﻿public class Solution
+{
+    public int SearchInsert(int[] nums, int target)
+    {
+        int l = 0, g = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == target) return i;
+
+            if (nums[i] < target) l = i + 1;
+
+            if (nums[i] > target)
+            {
+                g = i;
+                break;
+            }
+        }
+
+        if (l == 0 && g == 0) return 0;
+
+        if (g == 0) return nums.Length;
+
+        if (g > 0) return g;
+
+        return 0;
+    }
+}
